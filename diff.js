8f469939a3c2af5e41951626096934ba130a241f
spinachpasta <49642352+spinachpasta@users.noreply.github.com>
2019-06-15 00:52:39 +0900

    firstCommit

---
+++ diff.js
@@ -0,0 +1,64 @@
+// Standard Normal variate using Box-Muller transform.
+function randn_bm() {
+    var u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
+var arr5=[];
+var div5=[];
+var arr10=[];
+var div10=[];
+var samples=10000;
+var spread=0;
+function generateData(n){
+    var divs=[];
+    var allave=0;
+    spread=0;
+    for(let i=0;i<samples;i++){
+        var a=[];
+        var avg=0;
+        var div=0;
+        var num=n;
+        for(let m=0;m<num;m++){
+            var v=randn_bm();
+            a.push(randn_bm());
+        }
+        for(let m=0;m<num;m++){
+            avg+=a[m]/num;
+        }
+        divs.push(avg);
+        allave+=avg/samples;
+    }
+    for(let i=0;i<samples;i++){
+        var diff=divs[i]-allave;
+        spread+=diff*diff/samples;
+    }
+    
+    return divs;
+}
+
+function showData(n){
+    var datas=generateData(n);
+
+    var trace = {
+        x: datas,
+        type: 'histogram',
+    };
+    var data = [trace];
+    
+    var title=document.createElement("h1");
+    title.innerHTML=n+"spread:"+spread;
+    document.body.appendChild(title);
+    
+    var elem=document.createElement("div");
+    elem.id="aaa"+n;
+    document.body.appendChild(elem);
+    Plotly.newPlot("aaa"+n, data);
+}
+
+showData(5);
+showData(10);
+showData(100);
+showData(1000);
+showData(5000);
---
+++ main2.js
@@ -0,0 +1,108 @@
+// Standard Normal variate using Box-Muller transform.
+function randn_bm() {
+    var u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    return 1+Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
+var arr5=[];
+var div5=[];
+var arr10=[];
+var div10=[];
+var samples=10000;
+
+function generateData(n,k){
+    var divs=[];
+    for(let i=0;i<samples;i++){
+        var a=[];
+        var avg=0;
+        var div=0;
+        var num=n;
+        for(let m=0;m<num;m++){
+            var v=randn_bm();
+            a.push(randn_bm());
+        }
+        for(let m=0;m<num;m++){
+            avg+=a[m]/num;
+        }
+        for(let m=0;m<num;m++){
+            var diff=a[m]-avg;
+            div+=diff*diff/(num-k);
+        }
+        divs.push(Math.sqrt(div));
+    }
+    return divs;
+}
+function stdEv(arr){
+    var avg=0;
+    for(let i=0;i<arr.length;i++){
+        avg+=arr[i]/arr.length;
+    }
+    var spread=0;
+    for(let i=0;i<arr.length;i++){
+        let diff=arr[i]-avg;
+        spread+=diff*diff/(arr.length-1);
+    }
+    return Math.sqrt(spread);
+}
+function Average(arr){
+    var avg=0;
+    for(let i=0;i<arr.length;i++){
+        avg+=arr[i]/arr.length;
+    }
+    return avg;
+}
+function showData(n){
+    for(var l=1;l<2;l++){
+        var datas=generateData(n,l);
+
+        var trace = {
+            x: datas,
+            type: 'histogram',
+            marker: {
+                color: '#faa'
+            },
+            xbins: {
+                end: 5.2, 
+                size: 0.01, 
+                start: 0
+            }
+        };
+        var layout = {
+            xaxis: {range: [0, 2.5]}
+        };
+        var data = [trace];
+
+        var title=document.createElement("h1");
+        title.innerHTML=n+"samples";
+        document.body.appendChild(title);
+
+        var subtitle=document.createElement("div");
+        subtitle.innerHTML="stdEv:"+stdEv(datas)+",Average:"+Average(datas);
+        document.body.appendChild(subtitle);
+        
+        
+        var elem=document.createElement("div");
+        elem.id="aaa"+n+"a"+l;
+        document.body.appendChild(elem);
+        Plotly.newPlot(elem.id, data,layout);
+    }
+}
+function addPageBreak(){
+    var elem=document.createElement("div");
+    elem.style.pageBreakAfter="always";
+    document.body.appendChild(elem);
+}
+//showData(1);
+
+showData(2);
+showData(3);
+addPageBreak();
+showData(4);
+showData(5);
+addPageBreak();
+showData(10);
+showData(20);
+addPageBreak();
+showData(50);
+showData(100);
